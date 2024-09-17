cb886dcd14877c9e186f1489415d50d531c6fe36
jadedeo <jadedeo@gmail.com>
2024-09-16 20:03:59 -0400

    cleanup + comments

--- solution-hw2/js/script.js
+++ solution-hw2/js/script.js
@@ -1,5 +1,50 @@
 /**
- * Arrays of obejcts for lazing and pack size options
+ * Roll class
+ */
+class Roll {
+  constructor(type, basePrice) {
+    this.type = type;
+    this.glazing = "Keep Original"; //default
+    this.packSize = "1"; //default
+    this.basePrice = basePrice;
+    this.finalPrice = this.basePrice; //initially basePrice, but will be recalculated with glazing/pack size changes
+  }
+
+  updateGlazing(newGlazing) {
+    this.glazing = newGlazing;
+    this.updatePrice();
+  }
+
+  updatePackSize(newPackSize) {
+    this.packSize = newPackSize;
+    this.updatePrice();
+  }
+
+  updatePrice() {
+    // find the price adjustment that corresponds to the selected glaze
+    const glazingPrice = glazingOptions.find(
+      (g) => g.optionName === this.glazing
+    ).priceAdaptation;
+
+    // find the price adjustment that corresponds to the selected pack size
+    const packMultiplier = packSizeOptions.find(
+      (p) => p.optionName === this.packSize
+    ).priceAdaptation;
+
+    //calculate final price
+    this.finalPrice = (
+      (this.basePrice + glazingPrice) *
+      packMultiplier
+    ).toFixed(2);
+  }
+
+  getFinalPrice() {
+    return this.finalPrice;
+  }
+}
+
+/**
+ * Arrays of obejcts for glazing and pack size options
  */
 const glazingOptions = [
   {
@@ -40,53 +85,7 @@ const packSizeOptions = [
 ];
 
 /**
- * Roll class
- */
-class Roll {
-  constructor(type, basePrice) {
-    this.type = type;
-    this.glazing = "Keep Original";
-    this.packSize = "1";
-    this.basePrice = basePrice;
-    this.finalPrice = this.basePrice;
-  }
-
-  updateGlazing(newGlazing) {
-    console.log("updateGlazing", newGlazing);
-    this.glazing = newGlazing;
-    this.updatePrice();
-  }
-
-  updatePackSize(newPackSize) {
-    console.log("newPackSize", newPackSize);
-    this.packSize = newPackSize;
-    this.updatePrice();
-  }
-
-  updatePrice() {
-    const glazingPrice = glazingOptions.find(
-      (g) => g.optionName === this.glazing
-    ).priceAdaptation;
-    console.log("glazing: +", glazingPrice);
-
-    const packMultiplier = packSizeOptions.find(
-      (p) => p.optionName === this.packSize
-    ).priceAdaptation;
-    console.log("packSize: *", packMultiplier);
-
-    this.finalPrice = (
-      (this.basePrice + glazingPrice) *
-      packMultiplier
-    ).toFixed(2);
-  }
-
-  getFinalPrice() {
-    return this.finalPrice;
-  }
-}
-
-/**
- * Array of roll objects and cart array
+ * Array of roll objects
  */
 let rolls = [
   new Roll("Original", 2.49),
@@ -96,14 +95,20 @@ let rolls = [
   new Roll("Double-Chocolate", 3.99),
   new Roll("Strawberry", 3.99),
 ];
+
+/**
+ *  Initially empty cart array
+ */
 let cart = [];
 
 /**
  * Dynamically create glazing dropdown and pack size buttons from constants
  */
 function createGlazingSelect() {
+  //select all of the <select> elements meant to hold dropdown items
   const selects = document.querySelectorAll(".glazing");
   for (const select of selects) {
+    //within each, create <option>s that correspond to each of the glazing constants
     for (const option of glazingOptions) {
       const newOption = new Option(
         option.optionName,
@@ -115,25 +120,33 @@ function createGlazingSelect() {
 }
 
 function createPackSizeButtons() {
+  //select all of the containers meant to hold pack size buttons
   const packSizeContainers = document.querySelectorAll(".pack-size-container");
+
+  //loop through each of those containers
   packSizeContainers.forEach((container, containerIndex) => {
+    //and then loop through the pack size options
     packSizeOptions.forEach((packSize, packSizeIndex) => {
       const radioInput = document.createElement("input");
       radioInput.type = "radio";
+      //set up unique ids & names for each radio button dependent on the item/pack size container & pack size option
       radioInput.id = `prod${containerIndex + 1}-pack${packSize.optionName}`;
       radioInput.name = `prod${containerIndex + 1}-pack`;
       radioInput.value = packSize.priceAdaptation;
       radioInput.hidden = true;
 
+      //check all pack size 1s to start with
       if (packSize.optionName === "1") {
         radioInput.checked = true;
       }
 
+      //create & attach labels to each radio button
       const label = document.createElement("label");
       label.htmlFor = radioInput.id;
       label.textContent = packSize.optionName;
       label.className = "pack-btn";
 
+      //add new radio button & corresponding label to container
       container.appendChild(radioInput);
       container.appendChild(label);
     });
@@ -141,32 +154,46 @@ function createPackSizeButtons() {
 }
 
 /**
- * Attach event listeners to select options and radio buttons
+ * Add event listeners
  */
 function attachEventListeners() {
+  //select all glazing dropdowns, pack size radio buttons & 'add to cart' buttons
   const glazingSelects = document.querySelectorAll(".glazing");
   const packSizeButtons = document.querySelectorAll("[type='radio']");
   const addToCartButtons = document.querySelectorAll(".add-to-cart-cta");
 
   glazingSelects.forEach((select, index) => {
     select.addEventListener("change", (event) => {
+      //retrieve the roll for which the glazing select was changed
       const roll = rolls[index];
+      //update that roll's glazing instance variable with the new selection
       roll.updateGlazing(event.target.options[event.target.selectedIndex].text);
+
       displayUpdatedPrice(roll);
     });
   });
 
   packSizeButtons.forEach((button) => {
     button.addEventListener("change", (event) => {
+      //since each product has multiple pack size radio buttons, we need to identify which product we're referring to
       const rollIndex = parseInt(event.target.name.match(/\d+/)[0]) - 1;
+      //event.target.name will return something like prod4-pack for all pack size radio buttons of product 4
+      //we can use .match to extract the 4 (or any sequence of digits) from this name
+      //and [0] to access the first match from the returned array
+
+      //retrieve the roll for which the pack size was changed
       const roll = rolls[rollIndex];
+      //update that roll's packSize instance variable with the new selection
       roll.updatePackSize(event.target.nextSibling.textContent);
+
       displayUpdatedPrice(roll);
     });
   });
 
   addToCartButtons.forEach((button, index) => {
     button.addEventListener("click", () => {
+      //call addToCart when an 'add to cart' button is clicked
+      //pass the index (which represents the roll object/product we're referring to)
       addToCart(index);
     });
   });
@@ -188,7 +215,6 @@ function addToCart(index) {
   const newItem = rolls[index];
   cart.push(newItem);
 
-  console.log("cart:", cart);
   showPopup(newItem);
 
   const cartCount = document.getElementById("cart-count");
@@ -207,7 +233,7 @@ function getCartTotal() {
 }
 
 /**
- * Display popup
+ * Display popup, filling div with info for the newly added item
  */
 function showPopup(newItem) {
   const popup = document.getElementById("popup");
