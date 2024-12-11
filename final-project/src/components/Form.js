2e5f48e2554e702b8c611445662b0a287840255d
jadedeo <jadedeo@gmail.com>
2024-12-10 21:36:10 -0500

    add loading circle

--- final-project/src/components/Form.js
+++ final-project/src/components/Form.js
@@ -28,6 +28,7 @@ import {
   Chip,
   OutlinedInput,
   MenuItem,
+  CircularProgress,
 } from "@mui/material";
 import ChipInput from "./ChipInput";
 
@@ -48,19 +49,20 @@ const Form = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const [token, setToken] = useState("");
+
   const playlistType = useSelector((state) => state.playlistType);
   const title = useSelector((state) => state.title);
   const author = useSelector((state) => state.author);
   const character = useSelector((state) => state.character);
   const playlistTracks = useSelector((state) => state.playlistTracks);
-
   const [myMoods, setMyMoods] = useState(useSelector((state) => state.mood));
   const [keywords, setKeywords] = useState(
     useSelector((state) => state.keyword)
   );
-  const [token, setToken] = useState("");
 
   const [isValid, setIsValid] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const tokenFromStorage = window.localStorage.getItem("token");
@@ -111,6 +113,7 @@ const Form = () => {
 
   // perform playlist creation logic
   const performCreationLogic = async () => {
+    setIsLoading(true);
     console.log("--------------------------------");
     console.log(title, character, keywords, myMoods);
     console.log("--------------------------------");
@@ -156,6 +159,8 @@ const Form = () => {
       navigate("/playlist");
     } catch (error) {
       console.error("Error:", error);
+    } finally {
+      setIsLoading(false); // End loading regardless of success or failure
     }
   };
 
@@ -172,118 +177,128 @@ const Form = () => {
   return (
     <div id="form-container">
       <div id="form-content">
-        <FormControl component="fieldset">
-          <div id="playlist-type-form-control">
-            <FormLabel id="playlist-type-radio-label">
-              I would like to create a playlist for:
-            </FormLabel>
-            <RadioGroup
-              id="playlist-type-radio-group"
-              aria-labelledby="playlist-type-radio-label"
-              defaultValue="book"
-              name="playlist-type"
-              row
-              onChange={handlePlaylistTypeChange}
-              value={playlistType}
-            >
-              <FormControlLabel
-                className="playlist-type-radio"
-                value="book"
-                control={<Radio />}
-                label="A book"
-              />
-              <FormControlLabel
-                className="playlist-type-radio"
-                value="character"
-                control={<Radio />}
-                label="A book character"
-              />
-            </RadioGroup>
+        {isLoading ? (
+          <div id="loading-container">
+            <h4>Creating your playlist</h4>
+            <CircularProgress />
           </div>
-        </FormControl>
-
-        <div id="title-author-character-container">
-          <div id="title-author-container">
-            <FormControl component="fieldset" fullWidth>
-              <FormLabel id="title-input-label">Title*</FormLabel>
-              <TextField
-                variant="outlined"
-                aria-labelledby="title-input-label"
-                value={title}
-                onChange={(e) => dispatch(setTitle(e.target.value))}
-                fullWidth
-              />
+        ) : (
+          <>
+            <FormControl component="fieldset">
+              <div id="playlist-type-form-control">
+                <FormLabel id="playlist-type-radio-label">
+                  I would like to create a playlist for:
+                </FormLabel>
+                <RadioGroup
+                  id="playlist-type-radio-group"
+                  aria-labelledby="playlist-type-radio-label"
+                  defaultValue="book"
+                  name="playlist-type"
+                  row
+                  onChange={handlePlaylistTypeChange}
+                  value={playlistType}
+                >
+                  <FormControlLabel
+                    className="playlist-type-radio"
+                    value="book"
+                    control={<Radio />}
+                    label="A book"
+                  />
+                  <FormControlLabel
+                    className="playlist-type-radio"
+                    value="character"
+                    control={<Radio />}
+                    label="A book character"
+                  />
+                </RadioGroup>
+              </div>
             </FormControl>
-            <FormControl component="fieldset" fullWidth>
-              <FormLabel id="author-input-label">Author*</FormLabel>
-              <TextField
-                variant="outlined"
-                aria-labelledby="author-input-label"
-                value={author}
-                onChange={(e) => dispatch(setAuthor(e.target.value))}
-                fullWidth
-              />
-            </FormControl>
-          </div>
 
-          {playlistType === "character" && (
-            <FormControl component="fieldset" fullWidth>
-              <FormLabel id="character-input-label">Character Name*</FormLabel>
-              <TextField
-                variant="outlined"
-                aria-labelledby="character-input-label"
-                value={character}
-                onChange={(e) => dispatch(setCharacter(e.target.value))}
-                fullWidth
-              />
-            </FormControl>
-          )}
-        </div>
+            <div id="title-author-character-container">
+              <div id="title-author-container">
+                <FormControl component="fieldset" fullWidth>
+                  <FormLabel id="title-input-label">Title*</FormLabel>
+                  <TextField
+                    variant="outlined"
+                    aria-labelledby="title-input-label"
+                    value={title}
+                    onChange={(e) => dispatch(setTitle(e.target.value))}
+                    fullWidth
+                  />
+                </FormControl>
+                <FormControl component="fieldset" fullWidth>
+                  <FormLabel id="author-input-label">Author*</FormLabel>
+                  <TextField
+                    variant="outlined"
+                    aria-labelledby="author-input-label"
+                    value={author}
+                    onChange={(e) => dispatch(setAuthor(e.target.value))}
+                    fullWidth
+                  />
+                </FormControl>
+              </div>
 
-        <div id="mood-keyword-container">
-          <FormControl component="fieldset" fullWidth>
-            <FormLabel id="mood-input-label">Mood</FormLabel>
-            <Select
-              aria-labelledby="mood-input-label"
-              multiple
-              value={myMoods}
-              onChange={(e) => handleChange(e, "mood")}
-              input={<OutlinedInput />}
-              renderValue={(selected) => (
-                <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                  {selected.map((value) => (
-                    <Chip key={value} label={value} />
-                  ))}
-                </Box>
+              {playlistType === "character" && (
+                <FormControl component="fieldset" fullWidth>
+                  <FormLabel id="character-input-label">
+                    Character Name*
+                  </FormLabel>
+                  <TextField
+                    variant="outlined"
+                    aria-labelledby="character-input-label"
+                    value={character}
+                    onChange={(e) => dispatch(setCharacter(e.target.value))}
+                    fullWidth
+                  />
+                </FormControl>
               )}
+            </div>
+
+            <div id="mood-keyword-container">
+              <FormControl component="fieldset" fullWidth>
+                <FormLabel id="mood-input-label">Mood</FormLabel>
+                <Select
+                  aria-labelledby="mood-input-label"
+                  multiple
+                  value={myMoods}
+                  onChange={(e) => handleChange(e, "mood")}
+                  input={<OutlinedInput />}
+                  renderValue={(selected) => (
+                    <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                      {selected.map((value) => (
+                        <Chip key={value} label={value} />
+                      ))}
+                    </Box>
+                  )}
+                >
+                  {moods.map((mood) => (
+                    <MenuItem key={mood.name} value={mood.name}>
+                      {mood.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <FormControl>
+                <ChipInput
+                  label=""
+                  value={keywords}
+                  onChange={handleKeywordsChange}
+                  placeholder="Type and press enter"
+                />
+              </FormControl>
+            </div>
+
+            <Button
+              id="create-playlist-button"
+              variant="contained"
+              onClick={performCreationLogic}
+              disabled={!isValid}
             >
-              {moods.map((mood) => (
-                <MenuItem key={mood.name} value={mood.name}>
-                  {mood.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-
-          <FormControl>
-            <ChipInput
-              label=""
-              value={keywords}
-              onChange={handleKeywordsChange}
-              placeholder="Type and press enter"
-            />
-          </FormControl>
-        </div>
-
-        <Button
-          id="create-playlist-button"
-          variant="contained"
-          onClick={performCreationLogic}
-          disabled={!isValid}
-        >
-          Create Playlist
-        </Button>
-        {/* <p>Token: {token}</p> */}
+              Create Playlist
+            </Button>
+          </>
+        )}
       </div>
     </div>
   );
